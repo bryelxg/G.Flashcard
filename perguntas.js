630d09abae7b747f33c5c6f9423447925b619a30
bryelxg <00001093008611sp@al.educacao.sp.gov.br>
2024-11-28 20:32:33 -0300

    UPLOAD

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sobre IoT...',
+    'O que é a Internet das Coisas (IoT)?',
+    'A Internet das Coisas (IoT) refere-se à interconexão de dispositivos físicos à internet, permitindo que eles coletem, compartilhem e analisem dados para melhorar funcionalidades e criar automação.'
+)
+
+criaCartao(
+    'Sobre a IA...',
+    'Qual é a principal diferença entre Inteligência Artificial (IA) e Machine Learning (ML)?',
+    ' A Inteligência Artificial desenvolve sistemas capazes de realizar tarefas que simulam a inteligência humana, enquanto o Machine Learning é um subcampo da IA focado em treinar sistemas para aprender a partir de dados.'
+)
+
+criaCartao(
+    'Sobre computação...',
+    'O que significa o termo "computação em nuvem?',
+    'Computação em nuvem é a entrega de recursos de TI, como armazenamento, processamento e software, através da internet, eliminando a necessidade de infraestrutura física local.'
+)
+
+criaCartao(
+    'Sobre blockchain....',
+    'Para que serve a tecnologia de blockchain?',
+    'O blockchain é utilizado para registrar transações de forma descentralizada, segura e imutável, sendo a base para criptomoedas como o Bitcoin e útil em áreas como contratos inteligentes e rastreamento logístico.'
+) 
